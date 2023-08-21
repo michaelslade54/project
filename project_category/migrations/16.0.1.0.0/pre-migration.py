435ec9fcb844848d2b36c785a89d0ba57ae10973
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2023-08-21 15:09:46 +0930

    Make project type module merge

--- project_category/migrations/16.0.1.0.0/pre-migration.py
+++ project_category/migrations/16.0.1.0.0/pre-migration.py
@@ -2,4 +2,4 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.update_module_names(env.cr, [('project_category','project_type')],False)
+    openupgrade.update_module_names(env.cr, [('project_category','project_type')],True)
