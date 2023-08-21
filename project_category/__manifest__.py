fbfbf17e5bab6d6909e2ab232334362f49592317
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2023-08-21 14:39:30 +0930

    Fix name change of project_category module

---
+++ project_category/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Project Types",
+    "version": "16.0.1.0.0",
+    "category": "Project",
+    "license": "AGPL-3",
+    "depends": ["report_xlsx", "mrp"],
+    "installable": True,
+}
