cd1e57bfa5dacc398d32b1d41cefe84e938d425a
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2025-02-14 16:33:24 +1030

    Refactor for project_task_code migration

---
+++ project_task_code/migrations/17.0.1.0.0/pre-migration.py
@@ -0,0 +1,37 @@
+from openupgradelib import openupgrade
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+@openupgrade.migrate()
+def migrate(env, version):
+    
+    task_obj = env["project.task"]
+    sequence_obj = env["ir.sequence"]
+    # tasks = task_obj.search([('code','=',False)], order="id")
+    # for task_id in tasks.ids:
+    #     env.cr.execute(
+    #         "UPDATE project_task SET code = %s WHERE id = %s;",
+    #         (
+    #             sequence_obj.next_by_code("project.task"),
+    #             task_id,
+    #         ),
+    #     )
+    #     _logger.warning("ID CHANGED: " + str(task_id))
+
+    env.cr.execute(
+        f"""
+        SELECT id FROM project_task
+        WHERE code IS NULL
+        """
+    )
+
+    for (project_task_id,) in env.cr.fetchall():
+        env.cr.execute(
+            "UPDATE project_task SET code = %s WHERE id=%s;",
+            (
+                sequence_obj.next_by_code("project.task"),
+                project_task_id,
+            ),
+        )
