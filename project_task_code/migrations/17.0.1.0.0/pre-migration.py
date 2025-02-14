ca531b777f36f709dd824f43d59b1d0ef3a9983d
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2025-02-14 13:21:20 +1030

    Add project_task_code migration

---
+++ project_task_code/migrations/17.0.1.0.0/pre-migration.py
@@ -0,0 +1,15 @@
+from openupgradelib import openupgrade
+
+@openupgrade.migrate()
+def migrate(env, version):
+    task_obj = env["project.task"]
+    sequence_obj = env["ir.sequence"]
+    tasks = task_obj.search([('code','=',False)], order="id")
+    for task_id in tasks.ids:
+        env.cr.execute(
+            "UPDATE project_task SET code = %s WHERE id = %s;",
+            (
+                sequence_obj.next_by_code("project.task"),
+                task_id,
+            ),
+        )
