28eb88ef477efa0ad50cf79b1d87d7a02529e4e2
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2025-02-14 14:27:09 +1030

    Use Odoo migration utils to write project_task_code

--- project_task_code/migrations/17.0.1.0.0/pre-migration.py
+++ project_task_code/migrations/17.0.1.0.0/pre-migration.py
@@ -1,12 +1,11 @@
-from openupgradelib import openupgrade
+from odoo.upgrade import util
 
-@openupgrade.migrate()
-def migrate(env, version):
-    task_obj = env["project.task"]
-    sequence_obj = env["ir.sequence"]
+def migrate(cr, version):
+    task_obj = util.env(cr)["project.task"]
+    sequence_obj = util.env(cr)["ir.sequence"]
     tasks = task_obj.search([('code','=',False)], order="id")
     for task_id in tasks.ids:
-        env.cr.execute(
+        cr.execute(
             "UPDATE project_task SET code = %s WHERE id = %s;",
             (
                 sequence_obj.next_by_code("project.task"),
