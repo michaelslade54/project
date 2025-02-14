bb28d1f1efbadac74ade6d495a0202ef3e1a0c1d
michaelslade <48970841+michaelslade54@users.noreply.github.com>
2025-02-14 15:25:02 +1030

    Bug fix for project_task_code migration

--- project_task_code/migrations/17.0.1.0.0/pre-migration.py
+++ project_task_code/migrations/17.0.1.0.0/pre-migration.py
@@ -3,7 +3,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     task_obj = util.env(cr)["project.task"]
     sequence_obj = util.env(cr)["ir.sequence"]
-    tasks = task_obj.search([('code','=',False)], order="id")
+    tasks = task_obj.sudo().search([('code','=',False)], order="id")
     for task_id in tasks.ids:
         cr.execute(
             "UPDATE project_task SET code = %s WHERE id = %s;",
